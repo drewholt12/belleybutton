dadb2ae9b1e9d0647164379382d101614704405a
Andrew Holt <drewholt12@comcast.net>
2021-05-31 13:53:25 -0500

    update

--- plots.js
+++ plots.js
@@ -9,3 +9,21 @@ d3.json("samples.json").then(function(data){
     Object.entries(firstPerson).forEach(([key, value]) =>
       {console.log(key + ': ' + value);});
 });
+
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text(result.location);
+    });
+}
+
